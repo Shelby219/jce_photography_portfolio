b6a06e47813b2371385fee3d0c30e3244f78a2a8
ShelbyE <60080830+Shelby219@users.noreply.github.com>
2020-06-29 21:16:00 +1000

    added CRUD and categories

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,11 @@
 class Post < ApplicationRecord
     belongs_to :category
     validates :title, :content, :category_id, presence: true 
+    has_many :line_items, inverse_of: :order
 
+
+
+    
 end
 
 
