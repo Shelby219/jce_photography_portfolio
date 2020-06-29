48bda2b111c252dead2381f5fb985e92a1e0ff98
ShelbyE <60080830+Shelby219@users.noreply.github.com>
2020-06-29 21:19:41 +1000

    delete photo options, changed to posts

--- config/routes.rb
+++ config/routes.rb
@@ -2,31 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
  #website home page
- get "/", to:"pages#home", as:"home"
+ get "/home", to:"pages#home", as:"home"
  get "/about", to:"pages#about", as:"about"
  get "/contact", to:"pages#contact", as:"contact"
  get "/portfolio", to:"pages#portfolio", as:"portfolio"
-
- #  show a single photo
-#get "/photos/:id", to: "photos#show"
-
-#  create a new photo
- #post "/photos", to:"photos#create"
-
-# get a photo form page
- #get "/photos/new", to:"photos#new", as:"new_photo"
-
-#  Update a single photo
-#patch "/photos/:id", to: "photos#update"
-
-#  Edit a single photos
-#get "/photos/:id/edit", to: "photos#edit"
-
-#  Delete a single photos
-#delete "/photos/:id", to: "photos#delete"
-
-
-
 
 root 'posts#index'
 
@@ -38,13 +17,13 @@ root 'posts#index'
 #POST /posts matches the posts#create action
 #PATCH or PUT /posts/:id matches the posts#update action
 #DELETE /posts/:id matches the posts#destroy action
-get '/posts',             to: 'posts#index'
-get '/posts/new',         to: 'posts#new'
-get '/posts/:id',         to: 'posts#show'
-get '/posts/:id/edit',    to: 'posts#edit'
-post '/posts',            to: 'posts#create'
-patch '/posts/:id',       to: 'posts#update'
-delete '/posts/:id',      to: 'posts#destroy'
+#get '/posts',             to: 'posts#index'
+#get '/posts/new',         to: 'posts#new'
+#get '/posts/:id',         to: 'posts#show'
+#get '/posts/:id/edit',    to: 'posts#edit'
+#post '/posts',            to: 'posts#create'
+#patch '/posts/:id',       to: 'posts#update'
+#delete '/posts/:id',      to: 'posts#destroy'
 
 
 resources :posts 
