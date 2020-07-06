e2802b4f8759dfc3edd6c559bff550fea38e0368
ShelbyE <60080830+Shelby219@users.noreply.github.com>
2020-07-06 16:59:28 +1000

    home page edited

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,9 +2,9 @@ class Post < ApplicationRecord
     belongs_to :category
     validates :title, :content, :category_id, presence: true 
     has_many :line_items, inverse_of: :order
-    has_one_attached :image
+    has_one_attached :image  
     
 end
 
-
+  
 
